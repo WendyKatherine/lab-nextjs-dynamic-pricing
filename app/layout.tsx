214fe43e2b9f84c34fd600ff59c56f6828216154
Wendy Katherine Gomez <wendykatherinnegomezv@gmail.com>
2026-03-26 22:43:28 -0500

    style(ui): integrate retro design system theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,6 +26,8 @@ export default function RootLayout({
     <html
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      data-rt-theme="phosphor"
+      suppressHydrationWarning
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
